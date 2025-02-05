873d958b0e7d56e5db2282dde4437cb14d9e9ce7
Chermann-KING <codes.smiling@gmail.com>
2025-02-05 14:49:05 +0100

    🐛 Corrige la configuration du proxy pour l'API
    - Met à jour la configuration Vercel avec rewrites et headers

--- src/app/core/services/pokemon.service.ts
+++ src/app/core/services/pokemon.service.ts
@@ -8,8 +8,7 @@ import { Pokemon } from '../models/pokemon.model';
 })
 export class PokemonService {
   // private _baseUrl = 'https://pokeapi.co/api/v2';
-  // private _baseUrl = '/api/pokemon';
-  private _baseUrl = '/api/';
+  private _baseUrl = '/api/pokemon';
   private _allPokemons: Pokemons[] = [];
   private _observers: ((pokemons: Pokemons[]) => void)[] = [];
   private _totalCount = 0;
